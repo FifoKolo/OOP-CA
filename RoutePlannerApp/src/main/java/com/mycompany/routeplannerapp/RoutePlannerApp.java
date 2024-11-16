6ccc9dc0cd836aefaca7a836f20f47d04f7a2f4e
FifoKolo <148553297+FifoKolo@users.noreply.github.com>
2024-11-16 23:34:46 +0000

    Add files via upload

---
+++ RoutePlannerApp/src/main/java/com/mycompany/routeplannerapp/RoutePlannerApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.routeplannerapp;
+
+/**
+ *
+ * @author mncrf
+ */
+public class RoutePlannerApp {
+
+    public static void main(String[] args) {
+         MainGUI myGUI = new MainGUI();
+        myGUI.setVisible(true);
+    }
+}
